a5e1779315fe28ec2be0c9aa34523393675a3899
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-18 16:16:27 -0400

    4th attempt at creating a project

---
+++ readme.md
@@ -0,0 +1,6 @@
+Hello
+
+welcome to my read me.
+
+I am just learning how to use git which is 
+why this read me page is so short. 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("this is my 4th attempt, I am definitely getting the hang of this");
+        System.out.println(" I did this without looking at the instructions for the first time");
     }
 }
